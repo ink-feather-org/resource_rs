302dba199502ff4a5aa5e860ae4ef0fec5172d22
onestacked <chrisi.schrefl@gmail.com>
2022-08-12 16:45:37 +0200

    Improve old resource manager decl macro syntax

--- src/lib.rs
+++ src/lib.rs
@@ -62,73 +62,75 @@ pub trait GetResource<T> {
 
 #[macro_export]
 macro_rules! gen_res_mgr {
-    ($manager_name:ident: {$($name:ident: $type:ty),*$(,)?}) => {
+    ($manager_name:ident: {$($type:ty),*$(,)?}) => {
         use ::std::{collections::HashMap, any::{type_name, TypeId}};
-
-        #[derive(Debug)]
-        pub struct $manager_name {
-            registrar: HashMap<String, $crate::ResourceIndex>,
-            $($name: Vec<$type>,)*
-        }
-        impl $manager_name {
-            pub fn new() -> Self {
-                Self {
-                    registrar: HashMap::new(),
-                    $($name: Vec::new(),)*
-                }
+        use ::paste::paste;
+        paste!{
+            #[derive(Debug)]
+            pub struct $manager_name {
+                registrar: HashMap<String, $crate::ResourceIndex>,
+                $([<$type _collection>]: Vec<$type>,)*
             }
-            pub fn find(&self, key: &str) -> Result<$crate::ResourceIndex, $crate::GetError> {
-                if let Some(&re_idx) = self.registrar.get(key) {
-                    Ok(re_idx)
-                } else {
-                    Err($crate::GetError::NotFound{key: key.to_string()})
+            impl $manager_name {
+                pub fn new() -> Self {
+                    Self {
+                        registrar: HashMap::new(),
+                        $([<$type _collection>]: Vec::new(),)*
+                    }
                 }
-            }
-            pub fn get_type_name(type_id: TypeId) -> &'static str {
-                $(
-                    if type_id == TypeId::of::<$type>() {
-                        type_name::<$type>()
-                    } else
-                )*
-                {
-                    panic!("Unexpected type_id")
-                }
-            }
-        }
-        $(
-            impl $crate::RegisterResource<$type> for $manager_name {
-                fn register(&mut self, key: String, resource: $type) -> Result<$crate::ResourceIndex, $crate::RegisterError> {
-                    if let Some(re_idx) = self.registrar.get(&key) {
-                        Err(if re_idx.type_id() == TypeId::of::<$type>() {
-                            $crate::RegisterError::AlreadyExistingSameType{key} // Maybe overwrite existing value
-                        } else {
-                            $crate::RegisterError::AlreadyExistingOtherType{key, old_type: Self::get_type_name(re_idx.type_id()), new_type: type_name::<$type>()}
-                        })
-                    } else {
-                        let index = self.$name.len();
-                        let re_idx = $crate::ResourceIndex::new::<$type>(index);
-                        self.registrar.insert(key, re_idx);
-
-                        self.$name.push(resource);
+                pub fn find(&self, key: &str) -> Result<$crate::ResourceIndex, $crate::GetError> {
+                    if let Some(&re_idx) = self.registrar.get(key) {
                         Ok(re_idx)
-                    }
-                }
-            }
-            impl $crate::GetResource<$type> for $manager_name {
-                fn get_id(&self, key: &str) -> Result<&$type, $crate::GetError> {
-                    let index = self.find(key)?;
-                    self.get(index)
-                }
-                fn get(&self, index: $crate::ResourceIndex) -> Result<&$type, $crate::GetError> {
-
-                    if index.type_id() == TypeId::of::<$type>() {
-                        Ok(self.$name.get(index.index()).unwrap())
                     } else {
-                        Err($crate::GetError::WrongType{stored_type: Self::get_type_name(index.type_id()),
-                            expected_type: type_name::<$type>()})
+                        Err($crate::GetError::NotFound{key: key.to_string()})
+                    }
+                }
+                pub fn get_type_name(type_id: TypeId) -> &'static str {
+                    $(
+                        if type_id == TypeId::of::<$type>() {
+                            type_name::<$type>()
+                        } else
+                    )*
+                    {
+                        panic!("Unexpected type_id")
                     }
                 }
             }
-        )*
+            $(
+                impl $crate::RegisterResource<$type> for $manager_name {
+                    fn register(&mut self, key: String, resource: $type) -> Result<$crate::ResourceIndex, $crate::RegisterError> {
+                        if let Some(re_idx) = self.registrar.get(&key) {
+                            Err(if re_idx.type_id() == TypeId::of::<$type>() {
+                                $crate::RegisterError::AlreadyExistingSameType{key} // Maybe overwrite existing value
+                            } else {
+                                $crate::RegisterError::AlreadyExistingOtherType{key, old_type: Self::get_type_name(re_idx.type_id()), new_type: type_name::<$type>()}
+                            })
+                        } else {
+                            let index = self.[<$type _collection>].len();
+                            let re_idx = $crate::ResourceIndex::new::<$type>(index);
+                            self.registrar.insert(key, re_idx);
+
+                            self.[<$type _collection>].push(resource);
+                            Ok(re_idx)
+                        }
+                    }
+                }
+                impl $crate::GetResource<$type> for $manager_name {
+                    fn get_id(&self, key: &str) -> Result<&$type, $crate::GetError> {
+                        let index = self.find(key)?;
+                        self.get(index)
+                    }
+                    fn get(&self, index: $crate::ResourceIndex) -> Result<&$type, $crate::GetError> {
+
+                        if index.type_id() == TypeId::of::<$type>() {
+                            Ok(self.[<$type _collection>].get(index.index()).unwrap())
+                        } else {
+                            Err($crate::GetError::WrongType{stored_type: Self::get_type_name(index.type_id()),
+                                expected_type: type_name::<$type>()})
+                        }
+                    }
+                }
+            )*
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,62 @@
-#![feature(provide_any)]
-#![feature(downcast_unchecked)]
+//#![allow(incomplete_features)] // Maybe use min_specialization
+#![feature(min_specialization)]
 
-use resource_manager::{AnyProvider, AnyProviderImpl, AnyProviderInner, ConstProvider};
+use std::fmt::Debug;
+
+trait Tr<T: ?Sized> {
+    fn x(v: &T);
+}
+struct S;
+
+impl<T: Debug> Tr<T> for S {
+    fn x(v: &T) {
+        println!("Sized({}): {:?}", std::any::type_name::<T>(), v)
+    }
+}
+impl<T: Debug + ?Sized> Tr<T> for S {
+    default fn x(v: &T) {
+        println!("Unsized({}): {:?}", std::any::type_name::<T>(), v)
+    }
+}
+use resource_manager::{
+    gen_res_mgr, AnyProvider, AnyProviderImpl, AnyProviderInner, ConstProvider, GetResource,
+    Number, RegisterResource, Text,
+};
+
+gen_res_mgr!(ResourceManager: { Number, Text,});
 
 fn main() {
+    let mut res_mgr = ResourceManager::new();
+
+    res_mgr
+        .register(String::from("number1"), Number::new(3))
+        .unwrap();
+    res_mgr
+        .register(String::from("text1"), Text::new("Test text".to_string()))
+        .unwrap();
+    //res_mgr
+    //    .register(String::from("number1"), Text::new("Fail text".to_string()))
+    //    .unwrap();
+    //dbg!(&res_mgr);
+
+    let num: &Number = res_mgr.get_id("number1").unwrap();
+    println!("got: {}", num);
+
+    let text: &Text = res_mgr.get_id("text1").unwrap();
+    println!("got: {}", text);
+
+    let fail_text: Result<&Text, _> = res_mgr.get_id("number1");
+    println!("got Error: {}", fail_text.unwrap_err());
+
+    let idx = res_mgr.find("text1").unwrap();
+    let txt: &Text = res_mgr.get(idx).unwrap();
+    println!("idx_txt: {txt}");
+
+    let int: &i32 = &5;
+    S::x(int);
+    let str: &[u8] = &[1, 4, 7];
+    S::x(str);
+
     let mut prov = AnyProviderImpl::new();
     let mut test = Box::from(ConstProvider::new(69u8));
     test.register_prefix("val".to_string(), Box::from(AnyProviderImpl::new()));
